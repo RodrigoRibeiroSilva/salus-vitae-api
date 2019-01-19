7aac149337fe691751ebd9f77feb433346f85944
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-01-19 10:52:01 -0300

    Pasta dist removida

--- dist/common/environment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.environment = {
-    server: { port: process.env.PORT || 3000 },
-    db: { url: 'mongodb://dbasalus1:dbasalus1@ds111063.mlab.com:11063/salus-vitae' },
-    security: { saltRounds: process.env.saltRounds || 10 }
-};
