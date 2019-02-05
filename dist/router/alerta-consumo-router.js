c26441e5ec264f012ef86a962b0a49d75f77d9ff
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-02-04 21:18:03 -0300

    Corrigida rota do consumo

--- dist/router/alerta-consumo-router.js
+++ dist/router/alerta-consumo-router.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const alerta_consumo_controller_1 = require("../controller/alerta-consumo-controller");
 class AlertaConsumoRouter {
     applyRoutes(app) {
+        app.get('/', [alerta_consumo_controller_1.alertaConsumoController.findAll]);
         app.get('/alertaconsumos', [alerta_consumo_controller_1.alertaConsumoController.findAll]);
         app.get('/alertaconsumos/:id', [alerta_consumo_controller_1.alertaConsumoController.validateId, alerta_consumo_controller_1.alertaConsumoController.findById]);
         app.post('/alertaconsumos', [alerta_consumo_controller_1.alertaConsumoController.save]);
--- router/alerta-consumo-router.ts
+++ router/alerta-consumo-router.ts
@@ -6,6 +6,7 @@ import { alertaConsumoController } from '../controller/alerta-consumo-controller
 class AlertaConsumoRouter {
 
     applyRoutes(app: restify.Server){
+      app.get('/', [alertaConsumoController.findAll])
       app.get('/alertaconsumos',       [alertaConsumoController.findAll])
       app.get('/alertaconsumos/:id',   [alertaConsumoController.validateId, alertaConsumoController.findById])
       app.post('/alertaconsumos',      [alertaConsumoController.save])
