30a33ec454040ffc27b4f9d0d4bb71c3145a0cf8
RodrigoRibeiroSilva <rodrigo94.ribeiro.silva@gmail.com>
2019-02-04 21:26:30 -0300

    corrigido o ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,12 +7,12 @@ module.exports = {
     watch: true,
     merge_log: true,
     env: {
-      PORT: 8000,
+      PORT: process.env.PORT || 8000,
         DB_URL: "mongodb://dbasalus1:dbasalus1@ds111063.mlab.com:11063/salus-vitae",
         NODE_ENV: "development"
     },
     env_production: {
-      PORT: 8000,
+      PORT: process.env.PORT || 8000,
       NODE_ENV: "production"
     }
   }]
